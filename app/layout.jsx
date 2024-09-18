57cc1eb80127e3b4f493369b829a6c67430b1dec
MogeshG <gmogesh2003@gmail.com>
2024-09-18 18:32:40 +0530

    Fix: Suspense

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "PromptVerse",
@@ -11,16 +12,18 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
+        <Suspense>
+          <Provider>
+            <div className="main">
+              <div className="gradient" />
+            </div>
 
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
+            <main className="app">
+              <Nav />
+              {children}
+            </main>
+          </Provider>
+        </Suspense>
       </body>
     </html>
   );
--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Suspense, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
@@ -51,7 +51,6 @@ const EditPrompt = () => {
   };
 
   return (
-    <Suspense>
       <Form
         type="Edit"
         post={post}
@@ -59,7 +58,6 @@ const EditPrompt = () => {
         submitting={submitting}
         handleSubmit={updatePrompt}
       />
-    </Suspense>
   );
 };
 
